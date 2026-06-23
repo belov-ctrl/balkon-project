26d51896746b38f28121d2c0962d5bdb699c2095
Belov-Ctrl <belov@digitalstars.agency>
2026-06-23 18:22:40 +0400

    fix: добавлен IP сервера в белый список картинок Next.js

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,16 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: '83.222.18.44',
+        port: '1337',
+        pathname: '/uploads/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
